99a6c9a84d02bb433bc3cc8888b7f3aa989ac98e
Ernesto Camacho <ernesto.camachoa9805@gmail.com>
2020-04-04 09:13:00 -0500

    adicion del archivo tables.sql y plugin de jacoco

---
+++ tables.sql
@@ -0,0 +1,40 @@
+CREATE TABLE usuario(
+   `documento` bigint PRIMARY KEY,
+   `email` VARCHAR (50) UNIQUE,
+   `nombre` VARCHAR (50) NOT NULL,
+   `apellido` VARCHAR (50) NOT NULL,
+   `password` VARCHAR (50) NOT NULL,
+   `rol` VARCHAR (50) NOT NULL
+);
+
+CREATE TABLE iniciativa(
+   `id` int PRIMARY KEY,
+   `nombre` VARCHAR (50) NOT NULL,
+   `descripcion` VARCHAR (50) NOT NULL,
+   `estado` VARCHAR (50) NOT NULL,
+   `palabras_clave` VARCHAR (50)
+);
+
+CREATE TABLE registro(
+   `id` int PRIMARY KEY,
+   `fecha_registro` date not null,
+   `usuario` INTEGER REFERENCES usuario(documento) ,
+   `iniciativa` INTEGER references iniciativa(id) 
+);
+
+CREATE TABLE comentario(
+   `id` int PRIMARY KEY,
+   `fecha_comentario` date not null,
+   `contenido` VARCHAR (50) NOT NULL,
+   `usuario` INTEGER REFERENCES usuario(documento) ,
+   `iniciativa` INTEGER references iniciativa(id) 
+);
+
+CREATE TABLE votacion(
+   `id` int PRIMARY KEY,
+   `usuario` INTEGER REFERENCES usuario(documento) ,
+   `iniciativa` INTEGER references iniciativa(id) 
+);
+
+
+
